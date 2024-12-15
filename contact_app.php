a4433327ee3b8bb28f70f765afa4dfe8f612fbd6
rajatsaha18 <editorrajatdiu@gmail.com>
2024-12-15 12:25:08 +0600

    comment add

--- contact_app.php
+++ contact_app.php
@@ -2,19 +2,23 @@
 
 $contacts = [];
 
+// store contact array where input users data
 function addContact(array &$contacts, string $name, string $email,  string $phone)
 {
     $contacts[] = ['name' => $name, 'email' => $email, 'phone' => $phone];
 }
 
+// display users data
 function displayContact(array $contacts)
 {
+    // if empty contacts data
     if(empty($contacts))
     {
         echo "No contacts available";
     }
     else
     {
+        // show data loop wise users contact data
         foreach($contacts as $contact)
         {
             echo "Name:{$contact['name']} | Email:{$contact['email']} | Phone:{$contact['phone']}\n";
@@ -28,24 +32,27 @@ while(true)
     echo "\ncontact management menu\n";
     echo "1. Add Contact\n 2. View Contacts\n 3. Exit Contacts\n";
 
+    // user input
     $choice = (int)readline('Choose an option : ');
 
+    // check choice
     if($choice === 1)
     {
         $name   = readline("Your Name : ");
         $email  = readline("Your Email : ");
         $phone  = readline("Your Phone : ");
+        // check debug user correct choose data
         echo "Debug: Name = $name, Email = $email, Phone = $phone\n";
         addContact($contacts, $name, $email, $phone);
         echo "$name added successfully\n";
     }
     elseif($choice === 2)
     {
-        displayContact($contacts);
+        displayContact($contacts); // if choice 2 then show users display contact
     }
     elseif($choice === 3)
     {
-        echo "Exit the choice\n";
+        echo "Exit the choice\n"; // if choice 3 exit the choice
         break;
     }
     else
